d10cd394bdf08bf9cc47215e68f120b2587a1f9e
Valeryshchurik <Valeryschurik@gmail.com>
2017-04-15 23:53:52 +0300

    Add files via upload

---
+++ Analyser.m
@@ -0,0 +1,45 @@
+//
+//  Analyser.m
+//  TP6_1
+//
+//  Created by Admin on 15.04.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import "Analyser.h"
+
+@implementation Analyser
+- (void)foo:(NSString *)bar
+{
+    NSLog(bar);
+    NSArray *words = [bar componentsSeparatedByString:@" "];
+    NSMutableDictionary *statistics;
+    statistics = [[NSMutableDictionary alloc]init];
+    for (NSString* word in words)
+    {
+        NSNumber *repetitions = [statistics valueForKey:word];
+        if ([repetitions integerValue] > 0) {
+            [statistics setObject:[[NSNumber alloc] initWithLong:([repetitions integerValue] + 1)] forKey:word];
+        } else {
+            [statistics setObject:[[NSNumber alloc] initWithLong:1] forKey:word];
+        }
+    }
+    NSArray *sortedKeys = [statistics keysSortedByValueUsingComparator:^(id obj1, id obj2){
+        if([obj1 integerValue] < [obj2 integerValue])
+            return (NSComparisonResult)NSOrderedDescending;
+        if([obj1 integerValue] > [obj2 integerValue])
+            return (NSComparisonResult)NSOrderedAscending;
+        return (NSComparisonResult)NSOrderedSame;
+    }];
+    
+    int i = 0;
+    for(NSString* key in sortedKeys){
+        if(i > 4)
+            break;
+        NSNumber *n = [statistics valueForKey:key];
+        NSLog(@"%@: %ld", key, (long)[n integerValue]);
+        i++;
+    }
+}
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TP6_1
+//
+//  Created by Admin on 15.04.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Foundation/Foundation.h>
+#import "Analyser.h"
+int main(int argc, const char * argv[]) {
+    Analyser *analyser = [[Analyser alloc] init];
+    [analyser foo:@"Hello Hello World I am computer just a computer a a a a a a a"];
+    return 0;
+}
