750a0d071e944fa7e815f4535235708eb205800d
Valeryshchurik <Valeryschurik@gmail.com>
2017-04-16 22:38:08 +0300

    Add files via upload

---
+++ TP6_1/TP6_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TP6_1
+//
+//  Created by Admin on 15.04.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Foundation/Foundation.h>
+#import "Analyser.h"
+int main(int argc, const char * argv[]) {
+    Analyser *analyser = [[Analyser alloc] init];
+    [analyser foo:@"Hey hey la la la 42 2 42"];
+    return 0;
+}
