0d9bb060158bf39eb15015ff41176c1d84789465
Valeryshchurik <Valeryschurik@gmail.com>
2017-04-16 22:34:50 +0300

    Add files via upload

---
+++ TP6_4/TP6_4/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TP6_4
+//
+//  Created by Admin on 16.04.17.
+//  Copyright (c) 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
